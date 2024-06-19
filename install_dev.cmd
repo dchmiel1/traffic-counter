172ffd5844f80f8fb6891cd1b07c845610817ae1
dchmiel1 <chmielu.99@wp.pl>
2024-06-20 00:21:26 +0200

    Remove unused files

--- install_dev.cmd
+++
@@ -1,7 +0,0 @@
-echo Install OTAnalytics development environment.
-call install.cmd
-call venv\Scripts\activate
-pip install -r requirements-dev.txt --no-cache-dir%
-pip install -e .
-pre-commit install --install-hooks
-deactivate
